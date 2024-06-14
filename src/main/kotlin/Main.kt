811287a0141dae2ae9a86d78ba5f5efb82bc452e
seojw0124 <wjddn2000124@gmail.com>
2024-06-14 19:01:16 +0900

    feat: GlobalManager 클래스 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,20 +6,26 @@ import utils.FormatUtil
 import java.time.LocalDateTime
 
 fun main() {
+    val globalManager = init()
+    startKiosk(globalManager)
+}
 
+fun init(): GlobalManager {
     val userManager = UserManager()
     val categoryManager = CategoryManager(initCategoryData())
     val menuManager = MenuManager(initMenuData())
     val cartManager = CartManager()
     val orderManager = OrderManager()
 
-    var currentUser: UserInfo? = null
+    return GlobalManager(userManager, categoryManager, menuManager, cartManager, orderManager)
+}
 
+private fun startKiosk(globalManager: GlobalManager) {
     println("***** 정우 카페에 오신 것을 환영합니다 *****")
 
     while (true) {
-        currentUser = login(userManager)
-        runKiosk(currentUser, categoryManager, menuManager, cartManager, orderManager)
+        globalManager.currentUser = login(globalManager.userManager)
+        runKiosk(globalManager)
     }
 }
 
@@ -34,79 +40,77 @@ private fun login(userManager: UserManager): UserInfo {
             userManager.getLastUserId() + 1
         }
     userManager.saveUserInfo(UserInfo(userId, userName as String, money as Int))
-    if(userManager.setCurrentUser(userId)) {
-        println()
-        println("$userName 님 <정우 카페>에 오신 것을 환영합니다!!!")
-        println()
+    if (userManager.setCurrentUser(userId)) {
+        println("\n$userName 님 <정우 카페>에 오신 것을 환영합니다!!!\n")
     }
     return UserInfo(userId, userName, money)
 }
 
 private fun inputMyInfo(type: String): Any? {
     return when (type) {
-        "name" -> {
-            println("이름을 입력해주세요.")
-            while (true) {
-                try {
-                    val originName = readln()
-                    if (originName.first() != '_' && originName.first() != '!') { // orginName.first() 가 NoSuchElementException 발생 -> 이름 입력 안하면 catch로 넘어감
-                        return originName
-                    } else {
-                        println("이름을 다시 입력해주세요.")
-                    }
-                } catch (e: Exception) {
-                    println("이름을 다시 입력해주세요.")
-                }
-            }
-        }
-        "money" -> {
-            println("소지하고 계신 금액을 입력해주세요.")
-            while (true) {
-                try {
-                    val originMoney = readInt()
-                    if (originMoney > 0) {
-                        return originMoney
-                    } else {
-                        println("소지하고 계신 금액을 다시 입력해주세요")
-                    }
-                } catch (e: Exception) {
-                    println("소지하고 계신 금액을 다시 입력해주세요")
-                }
-            }
-        }
+        "name" -> inputName()
+        "money" -> inputMoney()
         else -> null
     }
 }
 
+fun inputName(): String {
+    println("이름을 입력해주세요.")
+    while (true) {
+        try {
+            val originName = readln()
+            if (originName.first() != '_' && originName.first() != '!') { // orginName.first() 가 NoSuchElementException 발생 -> 이름 입력 안하면 catch로 넘어감
+                return originName
+            } else {
+                println("이름을 다시 입력해주세요.")
+            }
+        } catch (e: Exception) {
+            println("이름을 다시 입력해주세요.")
+        }
+    }
+}
+
+fun inputMoney(): Int {
+    println("소지하고 계신 금액을 입력해주세요.")
+    while (true) {
+        try {
+            val originMoney = readInt()
+            if (originMoney > 0) {
+                return originMoney
+            } else {
+                println("소지하고 계신 금액을 다시 입력해주세요")
+            }
+        } catch (e: Exception) {
+            println("소지하고 계신 금액을 다시 입력해주세요")
+        }
+    }
+}
+
 private fun runKiosk(
-    currentUser: UserInfo,
-    categoryManager: CategoryManager,
-    menuManager: MenuManager,
-    cartManager: CartManager,
-    orderManager: OrderManager
+    globalManager: GlobalManager
 ) {
     var isExitKiosk = false
 
     while (!isExitKiosk) {
-        categoryManager.showCategory()
+        globalManager.categoryManager.showCategory()
         var isSelectedCategory = false
 
         while (!isSelectedCategory) {
             try {
                 val categoryId = readInt()
                 when (categoryId) {
-                    in 1..categoryManager.getItemCount() -> {
-                        showDetailMenuByCategory(currentUser, categoryManager, categoryId, menuManager, cartManager)
+                    in 1..globalManager.categoryManager.getItemCount() -> {
+                        showDetailMenuByCategory(globalManager, categoryId)
                         isSelectedCategory = true
                     }
                     8 -> {
-                        cartManager.showMyCart(currentUser)
+                        globalManager.currentUser?.let { globalManager.cartManager.showMyCart(it) }
                         print("메뉴를 선택하세요: ")
                         val selectedCartMenu = readInt()
                         when (selectedCartMenu) {
                             1 -> {
                                 isSelectedCategory = true
-                                addCartItemToOrder(currentUser, cartManager, orderManager)
+                                addCartItemToOrder(globalManager)
                             }
                             0 -> {
                                 isSelectedCategory = true
@@ -115,7 +119,7 @@ private fun runKiosk(
                             else -> println("없는 번호입니다. 다시 입력해주세요.")
                         }
                     }
-                    9 -> orderManager.showMyOrderList(currentUser.userId)
+                    9 -> globalManager.currentUser?.let { globalManager.orderManager.showMyOrderList(it.userId) }
                     0 -> {
                         isExitKiosk = true // 외부 루프를 종료하도록 설정
                         isSelectedCategory = true // 내부 루프를 종료하도록 설정
@@ -130,16 +134,16 @@ private fun runKiosk(
     }
 }
 
-private fun showDetailMenuByCategory(currentUser: UserInfo, categoryManager: CategoryManager, categoryId: Int, menuManager: MenuManager, cartManager: CartManager) {
-    val category = categoryManager.getCategoryItem(categoryId)
+private fun showDetailMenuByCategory(globalManager: GlobalManager, categoryId: Int) {
+    val category = globalManager.categoryManager.getCategoryItem(categoryId)
     category?.let {
-        menuManager.showDetailMenu(it)
-        selectMenuItem(currentUser, menuManager, categoryId, cartManager)
+        globalManager.menuManager.showDetailMenu(it)
+        selectMenuItem(globalManager, categoryId)
     }
 }
 
 /* 메뉴 골라서 장바구니 기능 */
-private fun selectMenuItem(currentUser: UserInfo, menuManager: MenuManager, categoryId: Int, cartManager: CartManager) {
+private fun selectMenuItem(globalManager: GlobalManager, categoryId: Int) {
     var isExitMenu = false
 
     while (!isExitMenu) {
@@ -150,13 +154,13 @@ private fun selectMenuItem(currentUser: UserInfo, menuManager: MenuManager, cate
             try {
                 val itemId = readInt()
                 when (itemId) {
-                    in 1..menuManager.getMenuItemCount(categoryId) -> {
-                        val item = menuManager.getMenuItem(categoryId, itemId)
+                    in 1..globalManager.menuManager.getMenuItemCount(categoryId) -> {
+                        val item = globalManager.menuManager.getMenuItem(categoryId, itemId)
                         item?.let {
                             isSelectedMenu = true
                             item.displayDetailInfo()
                             val quantity = getQuantity()
-                            checkAddMenuItemToCart(currentUser, cartManager, item, quantity)
+                            globalManager.currentUser?.let { it1 -> checkAddMenuItemToCart(it1, globalManager.cartManager, item, quantity) }
                         }
                     }
                     0 -> {
@@ -238,14 +242,14 @@ private fun getQuantity(): Int {
 }
 
 /* 결제하기 */
-fun addCartItemToOrder(currentUser: UserInfo, cartManager: CartManager, orderManager: OrderManager) {
-    val cartItemList = cartManager.getMyCartItemList(currentUser.userId)
+fun addCartItemToOrder(globalManager: GlobalManager) {
+    val cartItemList = globalManager.currentUser?.let { globalManager.cartManager.getMyCartItemList(it.userId) }
     println("cartItemList: $cartItemList")
     val orderId =
-        if (orderManager.isOrderListEmpty()) {
+        if (globalManager.orderManager.isOrderListEmpty()) {
             1
         } else {
-            orderManager.getLastOrderItemId() + 1
+            globalManager.orderManager.getLastOrderItemId() + 1
         }
 
     // 주문 시간은 현재 시간으로 설정(2024-06-14 00:00:00)
@@ -254,13 +258,13 @@ fun addCartItemToOrder(currentUser: UserInfo, cartManager: CartManager, orderMan
 
     val orderItem = OrderInfo(
         orderId,
-        currentUser.userId,
-        currentUser.userName,
-        cartItemList,
+        globalManager.currentUser!!.userId,
+        globalManager.currentUser!!.userName,
+        cartItemList!!,
         orderDate
     )
-    orderManager.addOrderItem(orderItem)
-    cartManager.clearMyCart()
+    globalManager.orderManager.addOrderItem(orderItem)
+    globalManager.cartManager.clearMyCart()
     println("결제가 완료되었습니다.")
 }
 
