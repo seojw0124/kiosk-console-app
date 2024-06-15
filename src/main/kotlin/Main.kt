d9e3c141832757970957fd7350a82cafcd1217ad
seojw0124 <wjddn2000124@gmail.com>
2024-06-16 05:13:16 +0900

    refactor: Kiosk 클래스 추가하고 리팩토링 진행

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,39 +7,216 @@ import java.time.LocalDateTime
 
 val money = 100000
 
-fun main() {
-    val globalManager = init()
-    println("***** 정우 카페에 오신 것을 환영합니다 *****")
+class Kiosk {
+    private lateinit var globalManager: GlobalManager
+    private var money: Int = 0
 
-    while (true) {
-        val money = inputMoney()
-        runKiosk(globalManager)
+    fun initKiosk() {
+        init()
+
+        println("***** 정우 카페에 오신 것을 환영합니다 *****")
+
+        money = inputMoney()
+        runKiosk()
     }
-}
 
-fun init(): GlobalManager {
-    val categoryManager = CategoryManager(initCategoryData())
-    val menuManager = MenuManager(initMenuData())
-    val cartManager = CartManager()
-    val orderManager = OrderManager()
+    private fun init() {
+        val categoryManager = CategoryManager(initCategoryData())
+        val menuManager = MenuManager(initMenuData())
+        val cartManager = CartManager()
+        val orderManager = OrderManager()
 
-    return GlobalManager(categoryManager, menuManager, cartManager, orderManager)
-}
+        globalManager = GlobalManager(categoryManager, menuManager, cartManager, orderManager)
+    }
 
-fun inputMoney(): Int {
-    println("소지하고 계신 금액을 입력해주세요.")
-    while (true) {
-        try {
-            val originMoney = readInt()
-            if (originMoney > 0) {
-                return originMoney
-            } else {
-                throw Exception()
+    fun inputMoney(): Int {
+        println("소지하고 계신 금액을 입력해주세요.")
+        while (true) {
+            try {
+                val originMoney = readInt()
+                if (originMoney > 0) {
+                    return originMoney
+                } else {
+                    throw Exception()
+                }
+            } catch (e: Exception) {
+                println("소지하고 계신 금액을 다시 입력해주세요")
             }
-        } catch (e: Exception) {
-            println("소지하고 계신 금액을 다시 입력해주세요")
         }
     }
+
+    private fun runKiosk() {
+        var isExitKiosk = false
+
+        while (true) {
+            globalManager.categoryManager.showCategory()
+            val selectedCategoryId = getSelectedCategoryId()
+
+            if (selectedCategoryId == 0) {
+                isExitKiosk = true
+                break
+            } else if (selectedCategoryId == 8) {
+                handleCart()
+                continue
+            }
+
+            handleMenu(selectedCategoryId)
+        }
+    }
+
+    private fun getSelectedCategoryId(): Int {
+        while (true) {
+            try {
+                val categoryId = readInt()
+                return when (categoryId) {
+                    in 1..globalManager.categoryManager.getItemCount() -> categoryId
+                    8 -> categoryId
+                    0 -> categoryId
+                    else -> throw IndexOutOfBoundsException()
+                }
+            } catch (e: IndexOutOfBoundsException) {
+                print("없는 카테고리입니다. 다시 입력해주세요. 카테고리: ")
+            }
+        }
+    }
+
+    private fun handleMenu(categoryId: Int) {
+        var isExitMenu = false
+
+        while (true) {
+            globalManager.menuManager.showDetailMenu(categoryId)
+            print("메뉴를 선택하세요(0: 뒤로가기): ")
+            val itemId = getSelectedMenuItemId(categoryId)
+            if (itemId == 0) break
+
+            val item = globalManager.menuManager.getMenuItem(categoryId, itemId)
+            item?.let {
+                isExitMenu = true
+                it.displayDetailInfo()
+                val quantity = getQuantity()
+                addItemToCart(it, quantity)
+            }
+        }
+    }
+
+    private fun getSelectedMenuItemId(categoryId: Int): Int {
+        while (true) {
+            try {
+                val itemId = readInt()
+                return when (itemId) {
+                    in 1..globalManager.categoryManager.getItemCount() -> itemId
+                    8 -> itemId
+                    0 -> itemId
+                    else -> throw IndexOutOfBoundsException()
+                }
+            } catch (e: IndexOutOfBoundsException) {
+                print("없는 메뉴입니다. 다시 입력해주세요. 메뉴: ")
+            }
+        }
+    }
+
+    private fun addItemToCart(item: MenuItem, quantity: Int) {
+        var isCancelAddToCart = false
+
+        while (!isCancelAddToCart) {
+            if (money < item.price * quantity) {
+                println("소지금이 부족합니다. 다른 상품을 선택해주세요. 현재 소지금: ${FormatUtil().decimalFormat(money)}원")
+            }
+
+            print("장바구니에 담으시겠습니까? (y/n): ")
+            while (true) {
+                try {
+                    val answer = readln().lowercase()
+                    when (answer) {
+                        "y" -> {
+                            val cartId =
+                                if (globalManager.cartManager.isCartEmpty()) {
+                                    1
+                                } else {
+                                    globalManager.cartManager.getLastCartItemId() + 1
+                                }
+                            val cartInfo = CartInfo(
+                                cartId,
+                                item.itemId,
+                                item.name,
+                                item.price,
+                                quantity
+                            )
+                            globalManager.cartManager.addCartItem(cartInfo, item.categoryId)
+                            isCancelAddToCart = true
+                            break
+                        }
+
+                        "n" -> {
+                            println("장바구니에 담기를 취소했습니다.")
+                            isCancelAddToCart = true
+                            break
+                        }
+
+                        else -> throw Exception()
+                    }
+                } catch (e: Exception) {
+                    print("잘못된 입력입니다. 다시 입력해주세요: ")
+                }
+            }
+        }
+    }
+
+    private fun handleCart() {
+        globalManager.cartManager.showMyCart(money)
+
+        print("메뉴를 선택하세요: ")
+        val selectedCartMenu = readInt()
+        when (selectedCartMenu) {
+            1 -> orderCartItem()
+            0 -> return
+            else -> println("없는 번호입니다. 다시 입력해주세요.")
+        }
+    }
+
+    private fun orderCartItem() {
+        val cartItemList = globalManager.cartManager.getMyCartItemList()
+        println("cartItemList: $cartItemList")
+        val orderId =
+            if (globalManager.orderManager.isOrderListEmpty()) {
+                1
+            } else {
+                globalManager.orderManager.getLastOrderItemId() + 1
+            }
+
+        // 주문 시간은 현재 시간으로 설정(2024-06-14 00:00:00)
+        val localDateTime = LocalDateTime.now().toString()
+        val orderDate = FormatUtil().getFormattedDate(localDateTime)
+        val orderItem = OrderInfo(orderId, cartItemList, orderDate)
+
+        globalManager.orderManager.addOrderItem(orderItem) // 나중에 cartManager에서 영수증 출력하기로 바꾸기
+        globalManager.cartManager.clearMyCart()
+        println("결제가 완료되었습니다.")
+    }
+
+    private fun getQuantity(): Int {
+        while (true) {
+            print("수량을 입력하세요: ")
+            val quantity = readInt()
+            if (quantity > 0) {
+                return quantity
+            } else {
+                println("수량은 1 이상이어야 합니다. 다시 입력해주세요.")
+            }
+        }
+    }
+}
+
+fun main() {
+//    val globalManager = init()
+//    println("***** 정우 카페에 오신 것을 환영합니다 *****")
+//
+//    while (true) {
+//        val money = inputMoney()
+//        runKiosk(globalManager)
+//    }
+    val kiosk = Kiosk()
+    kiosk.initKiosk()
 }
 
 private fun runKiosk(
